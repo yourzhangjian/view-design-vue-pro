321e60ca22c481e86ae31a129708c6e969c32979
zhangjian <507668447@qq.com>
2024-10-12 17:22:05 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,34 @@
 <template>
-  <Demo/>
+  <div class="demo">
+    <Button type="primary" @click="handleClick">Welcome!</Button>
+    <Tabs>
+        <TabPane label="macOS" icon="logo-apple">标签一的内容</TabPane>
+        <TabPane label="Windows" icon="logo-windows">标签二的内容</TabPane>
+        <TabPane label="Linux" icon="logo-tux">标签三的内容</TabPane>
+    </Tabs>
+  </div> 
 </template>
 
 <script>
-import Demo from '@/components/Demo.vue'
-
 export default {
   name: 'HomeView',
   components: {
-    Demo
+  },
+  setup() {
+  },
+  methods: {
+    handleClick() {
+      this.$Message.info('Welcome to View UI Plus Demo!')
+    }
   }
 }
 </script>
+
+<style lang="less" scoped>
+.demo{
+  color: lightcoral;
+  font-size: @font-size;
+  text-align: center;
+  margin: 300px 0 0 0;
+}
+</style>
